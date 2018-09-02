4fa24b1d14f68d957145d23c382370642498d8a5
Aurolux <36804393+Aurolux@users.noreply.github.com>
2018-09-02 10:59:31 -0500

    Update Factorial.py

--- Math/Factorial.py
+++ Math/Factorial.py
@@ -1,11 +1,12 @@
+# Control over script ending
 from sys import exit
+# Gets factorial function
 from math import factorial
 
+# Gets factorial number
 number = int(input("Enter the number you'd like to take the factorial of: "))
 if number <= 0:
     exit("Please enter a valid number.")
 
-
 factorial = factorial(number)
 print("The factorial is: ", factorial )
-
