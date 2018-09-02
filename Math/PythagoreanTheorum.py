254d0d93c07231bfce58212795cffcdef737f0e1
Aurolux <36804393+Aurolux@users.noreply.github.com>
2018-09-01 21:56:08 -0500

    Update PythagoreanTheorum.py

--- Math/PythagoreanTheorum.py
+++ Math/PythagoreanTheorum.py
@@ -1,5 +1,5 @@
 # Required for math functions
-import math
+from math import sqrt
 # Required for system control
 import sys
 
@@ -8,7 +8,7 @@ a = float(input('Please choose a length for side A of a right triangle: '))
 b = float(input('Please choose a length for side B of a right triangle: '))
 
 # Pythagorean Theorum used to calculate length of hypotenuse c
-c = math.sqrt(a**2+b**2)
+c = sqrt(a**2+b**2)
 
 # Print length c
 print("The length of your right triangle's hypotenuse is: ", c)
